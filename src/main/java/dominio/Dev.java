4e313ea7341083eeb593c411519939081369266f
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2022-03-22 16:40:01 -0400

    Concluindo a criação das classes do domínio

--- src/main/java/dominio/Dev.java
+++ src/main/java/dominio/Dev.java
@@ -2,17 +2,36 @@ package dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
 
     private String nome;
-    private Set<Conteudo> conteudosEscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void increverBootcamp(Bootcamp bootcamp){}
-    public void progredir(){}
-    public void calcularTotalXP(){}
+    public void increverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo");
+        }
+    }
+
+    public double calcularTotalXP(){
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXP)
+                .sum();
+    }
 
     public String getNome() {
         return nome;
@@ -22,12 +41,12 @@ public class Dev {
         this.nome = nome;
     }
 
-    public Set<Conteudo> getConteudosEscritos() {
-        return conteudosEscritos;
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
     }
 
-    public void setConteudosEscritos(Set<Conteudo> conteudosEscritos) {
-        this.conteudosEscritos = conteudosEscritos;
+    public void setconteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
     }
 
     public Set<Conteudo> getConteudosConcluidos() {
@@ -43,11 +62,11 @@ public class Dev {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosEscritos, dev.conteudosEscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudosEscritos, conteudosConcluidos);
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
